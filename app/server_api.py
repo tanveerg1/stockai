927a3851d8cae1012e1fa2534492c70ec1edc234
tanveerg1 <ghatourha.tanveer@gmail.com>
2025-05-07 18:27:47 -0400

    new changes

---
+++ app/server_api.py
@@ -0,0 +1,21 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+
+# Get MongoDB credentials from environment variables
+db_username = os.getenv("MONGO_DB_USERNAME")
+db_password = os.getenv("MONGO_DB_PASSWORD")
+cluster_name = os.getenv("MONGO_DB_CLUSTER_NAME")
+app_name = os.getenv("MONGO_DB_APP_NAME")   
+
+uri = f"mongodb+srv://{db_username}:{db_password}@stockaicluster.{cluster_name}.mongodb.net/?retryWrites=true&w=majority&appName={app_name}"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
